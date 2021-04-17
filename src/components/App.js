c2406e569c30b89153ab646de544dc4d5dd9e342
Anton Golosnichenko <golosnichenko@artics.ru>
2021-04-17 03:52:24 +0300

    0.4 Роутер и стили

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,14 @@
+import { HashRouter as Router, Route } from "react-router-dom";
+import Details from "../routes/Details";
+import Home from "../routes/Home";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route path="/:id" component={Details} />
+    </Router>
+  );
 }
 
 export default App;
