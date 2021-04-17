5550332f64f2396c26b75766363647645056ad60
Anton Golosnichenko <golosnichenko@artics.ru>
2021-04-17 17:32:08 +0300

    1.1 Запрос GET MOVIES

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,20 @@
-export default () => <h1>Главная страница</h1>;
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MOVIES = gql`
+  query {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+export default () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  if (loading) {
+    return "Загрузка...";
+  }
+  if (data?.movies) {
+    return data.movies.map((m) => <h1>{m.id}</h1>);
+  }
+};
