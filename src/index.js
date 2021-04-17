5670a7e45a86a7d84a3bcd4f62b66867d9ad3c08
Anton Golosnichenko <golosnichenko@artics.ru>
2021-04-17 04:25:09 +0300

    1.0 Клиент Apollo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import client from "./apollo";
+import { ApolloProvider } from "@apollo/client";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
